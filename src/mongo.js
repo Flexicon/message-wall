af139118654a98593482f6a3936a3da747c79c5f
Michal Repec <mikerepec@gmail.com>
2020-06-28 14:05:20 +0200

    Adjust docker setup and mongo connection

--- src/mongo.js
+++ src/mongo.js
@@ -2,10 +2,6 @@
 const mongoose = require('mongoose')
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://mongodb:27017/wall', {
   useNewUrlParser: true,
-  replicaSet: process.env.MONGODB_REPLICA_SET,
-  ssl: !!process.env.MONGODB_SSL,
-  retryWrites: !!process.env.MONGODB_RETRY_WRITES || true,
-  authSource: process.env.MONGODB_AUTH_SOURCE || 'admin',
   useUnifiedTopology: true,
   useFindAndModify: false,
 })
